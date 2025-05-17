5cb2422c5d1d13ec7408322a0f37e6566a98e8b4
Lindan Maulana <lindanstemga@gmail.com>
2025-05-17 21:36:59 +0700

    FEAT(home): added dynamic car item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-// import { signOut } from "@/auth";
-// import { Button } from "@/components/ui/button";
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 import { HomePage } from "@/features/client/home";
 
 export default async function Home() {
-  // const handleLogout = async () => {
-  //   "use server"
-  //   await signOut({redirectTo: "/auth/login"})
-  // }
+  const handleLogout = async () => {
+    "use server"
+    await signOut({redirectTo: "/auth/login"})
+  }
 
   return (
-    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    //   <form action={handleLogout}>
-    //     <Button type="submit" variant="destructive">
-    //       Logout
-    //     </Button>
-    //   </form>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <form action={handleLogout}>
+        <Button type="submit" variant="destructive">
+          Logout
+        </Button>
+      </form>
 
       <HomePage />
-    // </div>
+    </div>
   );
 }
--- src/features/client/home/car-rekomendation/dynamic-car.item.tsx
+++ src/features/client/home/car-rekomendation/dynamic-car.item.tsx
@@ -1,5 +1,6 @@
 "use client"
 import dynamic from "next/dynamic"
+
 const CardItemDynamic = dynamic(() => import("@/features/client/home/car-rekomendation/card-item"), {ssr: false, loading: () => <p>Loading...</p>})
 
 export const HomeCarRecomendationCardItemDynamic = () => {
